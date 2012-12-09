cc95b5dde3c1150ce06b91e0b36328e0d44bc490
rosejay <liny727@gmail.com>
2012-12-09 11:28:02 +0100

    PREVIEW!
    PREVIEW!

--- static/js/preview.js
+++ static/js/preview.js
@@ -12,25 +12,33 @@
 
 		for(var i = 0; i<=slideNum; i++){
 			var $box = $("<li class='preview' id='preview"+i+"'></li>");
-			var $dropzone = $("#dropzone"+i).clone();
+			var $dropzone = $(".slides #dropzone"+i).clone();
 			$dropzone.css("zoom",1/6.0);
 
-			var $canvasold = $("#dropzone"+i).find(".draw-box canvas");
-			var $canvasnew = $dropzone.find(".draw-box canvas");
-
-			for (var j = 0; j<$canvasnew.length; j++){ 
-				$canvasold[j] = $("#dropzone"+i).find(".draw-box canvas");
-				console.log($canvasold[j][0]);
-				var c = $canvasnew[j][0].getContext('2d');
-				//c = cloneCanvas($canvasold[j][0], c);
-				//$dropzone.find(".draw-box canvas")[j].replaceWith($canvasnew[j]);
-			}
 
 			if(i == index)
 				$box.addClass("sel");
 
 			$box.append($dropzone);
 			$(".preview-box").append($box);
+
+
+			var $canvasold = $(".slides #dropzone"+i).find(".draw-box div canvas");
+			var $canvasnew = $(".preview #dropzone"+i).find(".draw-box div canvas");
+
+
+			for (var j = 0; j<$canvasnew.length; j++){ 
+				$canvasold[j] = $(".slides #dropzone"+i).find(".draw-box div canvas");
+
+				console.log($canvasnew[j]);
+				$canvasnew[j].width = $canvasold[j][0].width;
+				$canvasnew[j].height = $canvasold[j][0].height;
+
+				console.log($canvasold[j][0]);
+				var c = $canvasnew[j].getContext('2d');
+				c = cloneCanvas($canvasold[j][0], c);
+				//$dropzone.find(".draw-box canvas")[j].replaceWith($canvasnew[j]);
+			}
 		}
 		
 	}
